bee6f93eebe72596ae36fc16e7370f45af7eb04d
Dylan McCapes <dmccapes@mac.com>
2020-04-12 10:49:15 -0700

    Use simpler poker game

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/poker.html');
+  res.sendFile(__dirname + '/holdem.html');
 });
 
 io.on('connection', function(socket){
