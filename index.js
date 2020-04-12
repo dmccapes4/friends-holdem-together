12e3d8438524c24ec3a52494641fc89cbffd5d90
Dylan McCapes <dmccapes@mac.com>
2020-04-12 14:56:57 -0700

    Show req

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ io.on('connection', function(socket){
 app.post('/users', function(req, res) {
   // io.sockets.in(req.body.user).emit('chat message', {msg: 'hello'});
   res.statusCode = 200;
-  res.send(req.body)
+  res.send(req.toString())
 });
 
 http.listen(port, function(){
