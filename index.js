0462850bb330e94b985c82266a60a6c6a386189b
Dylan McCapes <dmccapes@mac.com>
2020-04-12 17:45:10 -0700

    Use join only

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var users = []
+var users = {}
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
